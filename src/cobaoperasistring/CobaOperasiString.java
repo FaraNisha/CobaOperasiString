3996766feae9c7528b57a74b990d3574ed1a4dc9
WINDOWS 10 <WINDOWS 10@DESKTOP-VQO2UIQ>
2018-08-15 20:25:26 +0700

    Initial Commit

---
+++ src/cobaoperasistring/CobaOperasiString.java
@@ -0,0 +1,20 @@
+
+package cobaoperasistring;
+
+import java.util.Locale;
+import java.util.Scanner;
+public class CobaOperasiString {
+
+    private static int i;
+    public static void main(String[] args) {
+        
+        String identitas = "Fara Nisha Sukma Gustika / XRPL2 / 14";
+        System.out.println("Identitas : "+ identitas);
+        
+       
+        
+                
+        
+}
+}
+
